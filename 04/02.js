4681debeed3e45a20f0b41fbf12067b3fbb448c3
Arkadiusz Klein <arek.klein@limebrains.com>
2018-03-28 19:41:59 +0200

    04:02 use slice instead of substring to make it work with every str len

--- 04/02.js
+++ 04/02.js
@@ -1,4 +1,4 @@
 let text = 'Ala i As poszli w las';
 const re = /(\s[aiouwz][\s\t\n])/g;
-text = text.replace(re, (match, word) => word.substring(0, 2) + '&nbsp;');
+text = text.replace(re, (match, word) => word.slice(0, -1) + '&nbsp;');
 console.log(text);
