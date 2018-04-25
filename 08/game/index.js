25ae7543f106d5457bf8374d285c5d7d205447c9
aklein13 <arek.klein@gmail.com>
2018-04-25 13:28:52 +0200

    progress I guess

--- 08/game/index.js
+++ 08/game/index.js
@@ -1,10 +1,83 @@
-//jshint node: true, esversion: 6
+const rate = (code) => {
+  return (move) => {
+    if (code.length !== move.length) {
+      console.warn('Code and move lengths are not the same');
+      return;
+    }
+    const rating = {
+      white: {},
+      black: {},
+    };
+    move = move.filter((number, index) => {
+      if (code[index] === number) {
+        rating.black[index] = true;
+        return false;
+      }
+      return true;
+    });
+    move.forEach((number) => {
+      const foundIndex = code.indexOf(number);
+      if (foundIndex >= 0 && !rating.black[foundIndex] && !rating.white[foundIndex]) {
+        rating.white[foundIndex] = true;
+      }
+    });
+    return {
+      white: Object.keys(rating.white).length,
+      black: Object.keys(rating.black).length,
+    };
+  }
+};
+
+
+let games = {};
+
+const generateGame = (gameObject) => {
+  const {size, dim} = gameObject;
+  let game = [];
+  for (let i = 0; i < size; i++) {
+    game.push(Math.floor(Math.random() * dim));
+  }
+  return game;
+};
+
 const newGame = (req, res) => {
-  res.send("Do zrobienia!");
+  const gameParams = {};
+  console.log(req.session);
+  const {size, dim, max} = req.body;
+  if (size) {
+    gameParams.size = size;
+  }
+  else {
+    gameParams.size = 5;
+  }
+  if (dim) {
+    gameParams.dim = dim;
+  }
+  else {
+    gameParams.dim = 9;
+  }
+  if (max) {
+    gameParams.max = max;
+  }
+  else {
+    gameParams.max = 0;
+  }
+
+  const currentGame = generateGame(gameParams);
+  const result = {
+    game: currentGame,
+    params: gameParams,
+  };
+  const gameId = Object.keys(games).length;
+  res.session = gameId;
+  games[gameId] = result;
+  res.send(gameParams);
+  console.log(result);
 };
 
 const markAnswer = (req, res) => {
-  res.send("Też do zrobienia");
+  console.log(req.session);
+  console.log(req.body);
 };
 
 module.exports = {
--- 08/public/js/skrypt.js
+++ 08/public/js/skrypt.js
@@ -1,4 +1,27 @@
-/*jshint jquery: true, devel: true */
-$(function () {
-  alert('Zacznij grę!');
-});
+const routes = {
+  play: {method: 'POST', route: '/play'},
+  mark: {method: 'POST', route: '/mark'},
+};
+
+let response;
+
+const sendRequest = (route) => {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    const gameParams = JSON.parse(xhr.response);
+    console.log(gameParams);
+    return renderField(gameParams);
+  };
+  xhr.open(route.method, route.route, true);
+  return xhr;
+};
+
+const initGame = () => {
+  console.log('init');
+  sendRequest(routes.play);
+};
+
+const renderField = (params) => {
+  console.log(params);
+  sendRequest(routes.mark);
+};
